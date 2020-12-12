b8a90e53436c2e77d9bbc6568c29faa7b1061387
Fx-Colliot <fxcolliot@gmail.com>
2020-12-12 23:27:37 +0100

    First commit

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Border } from '../Style/StyledCards.js';
+
+export default function Cards() {
+  return (
+    <Border>
+      <div>
+        <h1>name</h1>
+        <h2>Poste</h2>
+        <img></img>
+      </div>
+      <div>
+        <p>taille</p>
+        <p>poids</p>
+        <p>age</p>
+        <p>country</p>
+      </div>
+    </Border>
+  );
+}
---
+++ src/Style/StyledCards.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Border = styled.div`
+  height: 25vh;
+  width: 15vw;
+  border: 2px solid black;
+  border-radius: 10px;
+  box-shadow: 10px 5px 5px red;
+`;
